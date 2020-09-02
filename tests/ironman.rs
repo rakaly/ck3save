068e4eaf4c757ad4315f28382520ad2b921de3f2
Nick Babcock <nbabcock19@hotmail.com>
2020-09-02 06:51:30 -0500

    Add test to ensure all tokens are known

--- tests/ironman.rs
+++ tests/ironman.rs
@@ -1,5 +1,5 @@
 #![cfg(ironman)]
-use ck3save::{Ck3Extractor, Encoding};
+use ck3save::{Ck3Extractor, Encoding, FailedResolveStrategy};
 
 mod utils;
 
@@ -15,6 +15,17 @@ fn test_ck3_binary_header() {
 fn test_ck3_binary_save() -> Result<(), Box<dyn std::error::Error>> {
     let data = utils::request("af_Munso_867_Ironman.ck3");
     let (save, encoding) = Ck3Extractor::extract_save(&data[..])?;
+    assert_eq!(encoding, Encoding::Binary);
+    assert_eq!(save.gamestate.meta_data.version, String::from("1.0.2"));
+    Ok(())
+}
+
+#[test]
+fn test_ck3_binary_save_tokens() -> Result<(), Box<dyn std::error::Error>> {
+    let data = utils::request("af_Munso_867_Ironman.ck3");
+    let (save, encoding) = Ck3Extractor::builder()
+        .with_on_failed_resolve(FailedResolveStrategy::Error)
+        .extract_save(&data[..])?;
     assert_eq!(encoding, Encoding::Binary);
     assert_eq!(save.gamestate.meta_data.version, String::from("1.0.2"));
     Ok(())
