5155e0bb3eb7f901c42d3683c3e94fd2db25c9a5
Nick Babcock <nbabcock19@hotmail.com>
2020-09-03 07:01:51 -0500

    Support ck3 autosaves and zero copy header deserialization
    Previously I only assumed there were two save formats. Turns out there
    are three: autosaves are a format different from standard and ironman
    saves. Autosaves are binary uncompressed gamestate data with the usual
    save id header. This commit adds support for autosave files.
    
    Since every format has the header already inflated. This commit allows
    one to zero copy deserialize the header. Thus I split the header type
    into HeaderOwned and HeaderBorrowed. These will need to be split for
    every field. Only time will tell if that ends being a worthwhile
    tradeoff (eg: allocation from owned data causes a significant slowdown).
    
    In this commit I also learned that the rust zip library doesn't need to
    be given data that starts at the beginning of the zip signature. Any
    junk at the start of the file (in this case, the CK3 header) can be
    unzipped with native tools:
    
    ```
    $ unzip af_Munso_867_Ironman.ck3
    warning [af_Munso_867_Ironman.ck3]:  28653 extra bytes at beginning or within zipfile
      (attempting to process anyway)
    ```
    
    Last but not least, requiring all the data to be in memory to
    deserialize the game state is a bit restrictive, so this commit also
    drops the requirement to only a `Read + Seek` implementation

---
+++ src/models/header.rs
@@ -0,0 +1,24 @@
+use serde::Deserialize;
+use std::borrow::Cow;
+
+#[derive(Debug, Deserialize)]
+pub struct HeaderOwned {
+    pub meta_data: MetadataOwned,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct HeaderBorrowed<'a> {
+    #[serde(borrow)]
+    pub meta_data: MetadataBorrowed<'a>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct MetadataOwned {
+    pub version: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct MetadataBorrowed<'a> {
+    #[serde(borrow)]
+    pub version: Cow<'a, str>,
+}
--- tests/samples.rs
+++ tests/samples.rs
@@ -1,4 +1,5 @@
 use ck3save::{Ck3Extractor, Encoding};
+use std::io::Cursor;
 
 mod utils;
 
@@ -6,15 +7,26 @@ mod utils;
 fn test_ck3_text_header() {
     let data = include_bytes!("fixtures/header.txt");
     let (header, encoding) = Ck3Extractor::extract_header(&data[..]).unwrap();
-    assert_eq!(encoding, Encoding::Text);
+    assert_eq!(encoding, Encoding::TextZip);
     assert_eq!(header.meta_data.version, String::from("1.0.2"));
+}
+
+#[test]
+fn test_ck3_text_header_borrowed() {
+    let data = include_bytes!("fixtures/header.txt");
+    let (header, encoding) = Ck3Extractor::builder()
+        .extract_header_borrowed(&data[..])
+        .unwrap();
+    assert_eq!(encoding, Encoding::TextZip);
+    assert_eq!(header.meta_data.version, "1.0.2");
 }
 
 #[test]
 fn test_ck3_text_save() -> Result<(), Box<dyn std::error::Error>> {
     let data = utils::request("Jarl_Ivar_of_the_Isles_867_01_01.ck3");
-    let (save, encoding) = Ck3Extractor::extract_save(&data[..])?;
-    assert_eq!(encoding, Encoding::Text);
-    assert_eq!(save.gamestate.meta_data.version, String::from("1.0.2"));
+    let reader = Cursor::new(&data[..]);
+    let (save, encoding) = Ck3Extractor::extract_save(reader)?;
+    assert_eq!(encoding, Encoding::TextZip);
+    assert_eq!(save.meta_data.version, String::from("1.0.2"));
     Ok(())
 }
