3d72d220b4023d91c8e301c0e935b3f41cfb13f8
Clément Schreiner <clement@mux.me>
2023-06-07 09:52:28 +0200

    wip: add debug

--- src/bin/debug_save.rs
+++ src/bin/debug_save.rs
@@ -1,6 +1,7 @@
 use std::env;
 
 use ck3save::{models::Gamestate, Ck3File, EnvTokens};
+use serde::Deserialize;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
@@ -8,27 +9,36 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file = Ck3File::from_slice(&data)?;
     let mut zip_sink = Vec::new();
     let file = file.parse(&mut zip_sink)?;
-    let save: Gamestate = file.deserializer(&EnvTokens).deserialize()?;
-    println!("{:#?}", save.meta_data.version);
-    println!("{:#?}", save.played_character.character);
-    println!("{:#?}", save.living.get(&save.played_character.character));
-    let traits = save
-        .living
-        .get(&save.played_character.character)
-        .unwrap()
-        .traits
-        .clone()
-        .unwrap();
-    let trait_strings = traits
-        .iter()
-        .map(|t| save.traits_lookup[*t].clone())
-        .collect::<Vec<String>>();
-    println!(
-        "{:#?}",
-        traits
-            .iter()
-            .map(|t| save.traits_lookup[*t].clone())
-            .collect::<Vec<String>>()
-    );
+    let deserializer = file.deserializer(&EnvTokens);
+    let result: Result<Gamestate, _> = serde_path_to_error::deserialize(deserializer);
+    match result {
+        Ok(_) => panic!("expected a type error"),
+        Err(err) => {
+            let path = err.path().to_string();
+            assert_eq!(path, "dependencies.serde.version");
+        }
+    }
+    // let save: Gamestate = deserializer.deserialize()?;
+    // println!("{:#?}", save.meta_data.version);
+    // println!("{:#?}", save.played_character.character);
+    // println!("{:#?}", save.living.get(&save.played_character.character));
+    // let traits = save
+    //     .living
+    //     .get(&save.played_character.character)
+    //     .unwrap()
+    //     .traits
+    //     .clone()
+    //     .unwrap();
+    // let trait_strings = traits
+    //     .iter()
+    //     .map(|t| save.traits_lookup[*t].clone())
+    //     .collect::<Vec<String>>();
+    // println!(
+    //     "{:#?}",
+    //     traits
+    //         .iter()
+    //         .map(|t| save.traits_lookup[*t].clone())
+    //         .collect::<Vec<String>>()
+    // );
     Ok(())
 }
