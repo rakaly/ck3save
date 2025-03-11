865b68f05c747d6fb5eb6176f29222cca67ddf0e
Nick Babcock <nbabcock19@hotmail.com>
2025-03-11 07:56:03 -0500

    Fix incorrect argument for file path in debug example

--- src/bin/debug_save.rs
+++ src/bin/debug_save.rs
@@ -4,7 +4,7 @@ use ck3save::{models::Gamestate, BasicTokenResolver, Ck3File};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
-    let file = std::fs::File::open(&args[0])?;
+    let file = std::fs::File::open(&args[1])?;
     let mut file = Ck3File::from_file(file)?;
     let file_data = std::fs::read("assets/ck3.txt").unwrap_or_default();
     let resolver = BasicTokenResolver::from_text_lines(file_data.as_slice())?;
